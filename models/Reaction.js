cfcb54fba12a0be17b1dae575d243867c6b9f6ac
Sarah Wesley <srwesley722@gmail.com>
2023-10-23 22:25:44 -0500

    Added the Reaction and Thought js schema files under the models folder

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+const { timeStamp } = require("console");
+const { Schema, Types } = require("mongoose");
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timeStamp => new Date(timestamp).toLocaleDateString()
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+module.exports = reactionSchema;
