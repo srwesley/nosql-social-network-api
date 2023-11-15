1bd7176a55093101d2568e086735ed7a9377c125
Sarah Wesley <srwesley722@gmail.com>
2023-11-14 19:50:49 -0600

    Made adjustment to the connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,10 @@
 // Imports the mongoose library
-const mongoose = require("mongoose");
+const { connect, connection } = require("mongoose");
 
-// Connects to the MongoDB database using the MongoDB URI provided in the environment variables or using the default URI if the environment variable isn't set
-mongoose.connect
+// Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume you're running this app locally
+const connectionString =
+    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/socialDB";
+
+    connect(connectionString);
+
+module.exports = connection;
